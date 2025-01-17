700fd493c92c95c0789c737f6336ee21b710397d
b.zamani@renolos.com <b.zamani@renolos.com>
2025-01-17 10:00:08 +0100

    firstore connected

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,  
+    firebaseConfig: {
+        apiKey: "AIzaSyDzujjxiWG9KYKeVKK4SnU8hJu6C0TgCtA",
+        authDomain: "ang-blog-703c8.firebaseapp.com",
+        projectId: "ang-blog-703c8",
+        storageBucket: "ang-blog-703c8.firebasestorage.app",
+        messagingSenderId: "372617340381",
+        appId: "1:372617340381:web:a842d94f384e3ed2904234"
+    }
+  };
